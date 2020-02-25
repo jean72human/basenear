861df090a0ebf2f45b833802491879b482678bf1
Gbetondji Dovonon <jsadovonon@gmail.com>
2020-02-25 10:28:09 +0000

    changed beta

--- basenear/constant.py
+++ basenear/constant.py
@@ -8,8 +8,8 @@ class Constant:
     # Searcher
 
     MAX_MODEL_NUM = 1000
-    BETA = 2.576
-    #BETA = 0
+    #BETA = 2.576
+    BETA = 0
     KERNEL_LAMBDA = 1.0
     T_MIN = 0.0001
     N_NEIGHBOURS = 8
@@ -28,9 +28,9 @@ class Constant:
     # ModelTrainer
 
     DATA_AUGMENTATION = False
-    MAX_ITER_NUM = 100
+    MAX_ITER_NUM = 200
     MIN_LOSS_DEC = 1e-4
     MAX_NO_IMPROVEMENT_NUM = 3
     MAX_BATCH_SIZE = 256
     LIMIT_MEMORY = False
-    SEARCH_MAX_ITER = 100
+    SEARCH_MAX_ITER = 200
